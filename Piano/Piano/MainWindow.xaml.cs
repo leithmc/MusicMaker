ddbf6a465af5302a9c3e176b6bf45a5229f0082e
Leith McCombs <Leith McCombs>
2016-05-26 22:24:06 -0700

    Adding Jacob's GUI work to repo as base project

---
+++ Piano/Piano/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Piano
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private String KeyName;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // for back programming.
+            BackCode backcode = new BackCode();
+            
+        }
+
+
+//Key Controller (Each Key has has its own parts)
+//
+//
+//White Keys
+
+
+        private void MouseOver(object sender, MouseEventArgs e)
+        {
+            ((Rectangle)sender).Fill = Brushes.Aqua;
+        }
+
+        private void MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((Rectangle)sender).Fill = Brushes.White;
+        }
+
+
+
+//Black Keys
+        private void MouseBlackOver(object sender, MouseEventArgs e)
+        {
+            ((Rectangle)sender).Fill = Brushes.Aqua;
+        }
+
+        private void MouseBlackLeave(object sender, MouseEventArgs e)
+        {
+            ((Rectangle)sender).Fill = Brushes.Black;
+        }
+
+
+
+//Capture Mouse Click
+        private void MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            ((Rectangle)sender).Fill = Brushes.Yellow;
+            KeyName = (((FrameworkElement)e.Source).Name);
+            Console.WriteLine(KeyName);
+        }
+
+
+
+        private void MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ((Rectangle)sender).Fill = Brushes.Aqua;
+            KeyName = "";
+            Console.WriteLine(KeyName);
+        }
+
+
+    }
+}
